931123fc95c836a7c86831f5289e102e812d875e
hongjunwan <dhks223@naver.com>
2017-10-23 18:08:52 +0900

    생성
    dao패키지 DBUtil 클래스 생성 - db 공통

--- src/dao/ContentDao.java
+++ src/dao/ContentDao.java
@@ -1,9 +1,17 @@
 package dao;
 
+import java.sql.Date;
+
 //10월 23일 테이블 Content
 //content_no(int auto_increment, primary key),title(varchar),read_count(int),
 //writer(varchar),write_time(date),main_img(varchar) 	
 
 public class ContentDao {
-
+	int content_no;
+	String title;
+	int read_count;
+	String writer;
+	Date write_time;
+	String main_img;
+	////////////////////////////////////////////////////////////////////////
 }
--- src/dao/ContentDetailDao.java
+++ src/dao/ContentDetailDao.java
@@ -4,5 +4,9 @@ package dao;
 //content_no(int)(외래키 참조하는 테이블 Content),day(int),content(varchar),path(varchar)
 
 public class ContentDetailDao {
-
+	int content_no;
+	int day;
+	String content;
+	String path;
+	//////////////////////////////////////////////////////////////////////////////////////
 }
