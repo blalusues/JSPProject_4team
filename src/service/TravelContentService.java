de1120c80eb216f4ad548b7fcded48cd1f26107f
hongjunwan <dhks223@naver.com>
2017-10-23 18:00:48 +0900

    약간 수정

--- src/service/TravelContentService.java
+++ src/service/TravelContentService.java
@@ -1,6 +1,6 @@
 package service;
 
-//준완,종훈 2017/10/23 생성
+//2017/10/23 생성
 //후기 내용 servlet
 
 public class TravelContentService {
--- src/servlet/TravelContentServlet.java
+++ src/servlet/TravelContentServlet.java
@@ -1,8 +1,39 @@
 package servlet;
 
-//준완,종훈 2017/10/23 생성
+//2017/10/23 생성
 //후기 내용 servlet
 
-public class TravelContentServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/content")
+public class TravelContentServlet extends HttpServlet {
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String task = request.getParameter("task");
+		String path = "";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("euc-kr");
+		String task = request.getParameter("task");
+		String path = "";
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
+	}
 }
