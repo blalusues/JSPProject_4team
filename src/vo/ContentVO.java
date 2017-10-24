77400410d5df7dfa2a7644a8d331ae045d8cfd4f
hongjunwan <dhks223@naver.com>
2017-10-24 15:44:16 +0900

    db작업
    servlet -> service (read) -> db
    db: contentSelect, contentDetailSelect,
          updateReadCount

--- src/vo/ContentVO.java
+++ src/vo/ContentVO.java
@@ -1,25 +1,28 @@
 package vo;
 
-import java.sql.Date;
+import java.util.Date;
 
 //10/23 »ý¼º
 
 public class ContentVO {
-	int content_no;
+	int content_no;  //auto
 	String title;
 	int read_count;
 	String writer;
 	Date write_time;
 	String main_img;
+	String location;
 
 	/////////////////////////////////////////////////////////////////////////////////////////////
-	public ContentVO(int content_no, String title, int read_count, String writer, Date write_time, String main_img) {
+	public ContentVO(int content_no, String title, int read_count, String writer, Date write_time, String main_img,
+			String location) {
 		this.content_no = content_no;
 		this.title = title;
 		this.read_count = read_count;
 		this.writer = writer;
 		this.write_time = write_time;
 		this.main_img = main_img;
+		this.location = location;
 	}
 
 	public ContentVO() {
@@ -73,11 +76,19 @@ public class ContentVO {
 	public void setMain_img(String main_img) {
 		this.main_img = main_img;
 	}
+	
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
 	@Override
 	public String toString() {
 		return "ContentVO [content_no=" + content_no + ", title=" + title + ", read_count=" + read_count + ", writer="
-				+ writer + ", write_time=" + write_time + ", main_img=" + main_img + "]";
+				+ writer + ", write_time=" + write_time + ", main_img=" + main_img + ", location=" + location + "]";
 	}
-
 }
