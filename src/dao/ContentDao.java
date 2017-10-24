19fcc8ff374b5bfb9ec74d7e9e5a0978a1b6f3f6
hongjunwan <dhks223@naver.com>
2017-10-24 11:20:37 +0900

    수정
    contentdao 수정

--- src/dao/ContentDao.java
+++ src/dao/ContentDao.java
@@ -1,5 +1,25 @@
 package dao;
 
-public class ContentDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class ContentDao {
+	// singleton
+	private static ContentDao instance;
+
+	public static ContentDao getInstance() {
+		if (instance == null)
+			instance = new ContentDao();
+		return instance;
+	}
+
+	private ContentDao() {
+	}
+
+	///////////////////////////////////////////////////////////
+	private Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	/////////////////////////////////////////////////////////
 }
--- src/service/TravelContentService.java
+++ src/service/TravelContentService.java
@@ -1,7 +1,9 @@
 package service;
 
+import dao.ContentDao;
+
 //2017/10/23 »ý¼º
 
 public class TravelContentService {
-	
+	private ContentDao dao = ContentDao.getInstance();
 }
