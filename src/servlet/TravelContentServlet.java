7dd021568b3c631464055bffac50c5f5b59b8fb9
rnsidgm <rnsidgm@users.noreply.github.com>
2017-10-30 18:53:41 +0900

    글쓰기 db요청 처리

--- src/servlet/TravelContentServlet.java
+++ src/servlet/TravelContentServlet.java
@@ -3,7 +3,11 @@ package servlet;
 //2017/10/23 생성
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -82,7 +86,7 @@ public class TravelContentServlet extends HttpServlet {
 			request.setAttribute("search", search);
 		} else if (task.equals("wirteForm")) {
 			// 글 쓰기 화면으로 갈 때(로그인 부분을 몰라서리...)
-
+			path = "write_form.jsp";
 		} else if (task.equals("updateForm")) {
 			HttpSession session = request.getSession();
 			String loginId = (String) session.getAttribute("loginId");
@@ -114,17 +118,33 @@ public class TravelContentServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		String task = request.getParameter("task");
 		String path = "";
-
+		
+		Enumeration<String> params = request.getParameterNames();
+		while(params.hasMoreElements()) {
+			System.out.println("--"+params.nextElement());
+		}
+		
 		// 글 작성 완료 후 submit 버튼 눌렀을 때
 		if (task.equals("write")) {
 			ContentVO content = new ContentVO();
 			List<ContentDetailVO> detailList = new ArrayList<>();
-
+			
 			content.setTitle(request.getParameter("title"));
-			content.setWriter(request.getParameter("writer"));
+			content.setWriter(String.valueOf(request.getSession().getAttribute("name")));
 			content.setLocation(request.getParameter("location"));
 			content.setMain_img(request.getParameter("main_img"));
-
+			
+//			Date start_date = null, end_date= null;
+//			try {
+//				start_date = (Date) new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("start_date"));
+//				end_date = (Date) new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("end_date"));
+//			} catch (ParseException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			content.setStart_date(request.getParameter("start_date"));
+			content.setEnd_date(request.getParameter("end_date"));
+//			content.setWrite_time(new Date(System.currentTimeMillis()));
 			// day 개수 (Day02까지 썼으면 day=2)
 			String dayStr = request.getParameter("day");
 			int day = 0;
