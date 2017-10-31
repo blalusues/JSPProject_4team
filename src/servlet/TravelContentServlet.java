8d3358fb1819bec2259f234d1df5de24bd33f22b
ilms9012 <31472934+ilms9012@users.noreply.github.com>
2017-10-31 16:51:13 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

--- src/servlet/TravelContentServlet.java
+++ src/servlet/TravelContentServlet.java
@@ -165,7 +165,7 @@ public class TravelContentServlet extends HttpServlet {
 					ContentDetailVO detail = new ContentDetailVO();
 
 					detail.setDay(i);
-					detail.setContent(request.getParameter("content" + i));
+					detail.setContent(mReq.getParameter("content" + i));
 					for (int x = 1; x < maxPath[i] + 1; x++) {
 						plusPath = plusPath + mReq.getParameter("loc" + i + "_" + x) + "%"
 								+ mReq.getParameter("sum" + i + "_" + x) + "%";
